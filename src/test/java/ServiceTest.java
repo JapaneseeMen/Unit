c66721b672feda3ea459beae8474344cf7db716b
ALEKSANDER <melsmaxms@gmail.com>
2023-04-04 17:09:33 +0700

    add JUnit4

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -1,43 +1,43 @@
-import static org.testng.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
 public class ServiceTest {
 
-        Service service = new Service();
-        @Test
-        public void remainFirstTest() {
-            Assert.assertEquals(service.remain(1000), 0);
-        }
-        @Test
-        public void remainSecondTest() {
-            Assert.assertEquals(service.remain(0), 1000);
-        }
+    Service service = new Service();
 
-        @Test
-        public void remainThirdTest() {
-            Assert.assertEquals(service.remain(1), 999);
-        }
-
-        @Test
-        public void remainFourthTest() {
-            Assert.assertEquals(service.remain(100), 900);
-        }
-
-        @Test
-        public void remainFifthTest() {
-            Assert.assertEquals(service.remain(900), 100);
-        }
-
-        @Test
-        public void remainSixthTest() {
-            Assert.assertEquals(service.remain(999), 1);
-        }
-
-        @Test
-        public void remainSeventhTest() {
-            Assert.assertEquals(service.remain(500), 500);
-        }
-
-
+    @Test
+    public void remainFirstTest() {
+        Assert.assertEquals(0, service.remain(1000));
     }
+
+    @Test
+    public void remainSecondTest() {
+        Assert.assertEquals(1000, service.remain(0));
+    }
+
+    @Test
+    public void remainThirdTest() {
+        Assert.assertEquals(999, service.remain(1));
+    }
+
+    @Test
+    public void remainFourthTest() {
+        Assert.assertEquals(900, service.remain(100));
+    }
+
+    @Test
+    public void remainFifthTest() {
+        Assert.assertEquals(100, service.remain(900));
+    }
+
+    @Test
+    public void remainSixthTest() {
+        Assert.assertEquals(1, service.remain(999));
+    }
+
+    @Test
+    public void remainSeventhTest() {
+        Assert.assertEquals(500, service.remain(500));
+    }
+}
+    
